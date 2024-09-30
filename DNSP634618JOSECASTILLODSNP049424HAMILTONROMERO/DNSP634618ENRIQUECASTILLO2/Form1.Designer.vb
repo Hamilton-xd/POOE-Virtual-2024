590ebe705ad37f12b28956351e6aa8f354993408
Hamilton Romero <hromeroperla79@gmail.com>
2024-09-29 18:28:38 -0600

    Bloque 2

---
+++ DNSP634618JOSECASTILLODSNP049424HAMILTONROMERO/DNSP634618ENRIQUECASTILLO2/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboTipounidad = New System.Windows.Forms.ComboBox()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.comboUnidadEntrada = New System.Windows.Forms.ComboBox()
+        Me.comboUnidadSalida = New System.Windows.Forms.ComboBox()
+        Me.txtValor = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnConvertir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboTipounidad
+        '
+        Me.ComboTipounidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboTipounidad.FormattingEnabled = True
+        Me.ComboTipounidad.Items.AddRange(New Object() {"Longitud", "Masa", "Tiempo", "Almacenamiento"})
+        Me.ComboTipounidad.Location = New System.Drawing.Point(336, 30)
+        Me.ComboTipounidad.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ComboTipounidad.Name = "ComboTipounidad"
+        Me.ComboTipounidad.Size = New System.Drawing.Size(177, 24)
+        Me.ComboTipounidad.TabIndex = 0
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultado.Location = New System.Drawing.Point(100, 266)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(130, 24)
+        Me.lblResultado.TabIndex = 1
+        Me.lblResultado.Text = "RESULTADO"
+        '
+        'comboUnidadEntrada
+        '
+        Me.comboUnidadEntrada.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboUnidadEntrada.FormattingEnabled = True
+        Me.comboUnidadEntrada.Items.AddRange(New Object() {"Kilómetros", "", "Centímetros ", "", "Milímetros", "", "Pulgadas", "", "Metros ", "Kilogramos", "", "Gramos", "Miligramos", "", "Libras", "Horas", "", "Minutos", "", "Segundos ", "", "Milisegundos", "Kilobytes", "", "Megabytes", "", "Gigabytes", "", "Bytes"})
+        Me.comboUnidadEntrada.Location = New System.Drawing.Point(336, 110)
+        Me.comboUnidadEntrada.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.comboUnidadEntrada.Name = "comboUnidadEntrada"
+        Me.comboUnidadEntrada.Size = New System.Drawing.Size(177, 24)
+        Me.comboUnidadEntrada.TabIndex = 2
+        '
+        'comboUnidadSalida
+        '
+        Me.comboUnidadSalida.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboUnidadSalida.FormattingEnabled = True
+        Me.comboUnidadSalida.Items.AddRange(New Object() {"Kilómetros", "", "Centímetros ", "", "Milímetros", "", "Pulgadas", "", "Metros ", "Kilogramos", "", "Gramos", "Miligramos", "", "Libras", "Horas", "", "Minutos", "", "Segundos ", "", "Milisegundos", "Kilobytes", "", "Megabytes", "", "Gigabytes", "", "Bytes"})
+        Me.comboUnidadSalida.Location = New System.Drawing.Point(336, 183)
+        Me.comboUnidadSalida.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.comboUnidadSalida.Name = "comboUnidadSalida"
+        Me.comboUnidadSalida.Size = New System.Drawing.Size(177, 24)
+        Me.comboUnidadSalida.TabIndex = 3
+        '
+        'txtValor
+        '
+        Me.txtValor.Location = New System.Drawing.Point(504, 266)
+        Me.txtValor.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtValor.Name = "txtValor"
+        Me.txtValor.Size = New System.Drawing.Size(147, 22)
+        Me.txtValor.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(100, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 24)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "TIPO:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(100, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 24)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "UNIDAD INICIAL"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(100, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(146, 24)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "UNIDAD FINAL"
+        '
+        'btnConvertir
+        '
+        Me.btnConvertir.Location = New System.Drawing.Point(267, 324)
+        Me.btnConvertir.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnConvertir.Name = "btnConvertir"
+        Me.btnConvertir.Size = New System.Drawing.Size(135, 38)
+        Me.btnConvertir.TabIndex = 8
+        Me.btnConvertir.Text = "CONVERTIR"
+        Me.btnConvertir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(729, 382)
+        Me.Controls.Add(Me.btnConvertir)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtValor)
+        Me.Controls.Add(Me.comboUnidadSalida)
+        Me.Controls.Add(Me.comboUnidadEntrada)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.ComboTipounidad)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboTipounidad As ComboBox
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents comboUnidadEntrada As ComboBox
+    Friend WithEvents comboUnidadSalida As ComboBox
+    Friend WithEvents txtValor As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnConvertir As Button
+End Class
---
+++ DNSP634618JOSECASTILLODSNP049424HAMILTONROMERO/DNSP634618ENRIQUECASTILLO2/Form1.vb
@@ -0,0 +1,189 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' DSNP634618 JOSE ENRIQUE CASTILLO BENAVIDES
+        ' DSNP049424 HAMILTON DANIEL ROMERO PERLA
+        ' Desactivar las listas de unidades hasta que se seleccione un tipo
+        comboUnidadEntrada.Enabled = False
+        comboUnidadSalida.Enabled = False
+    End Sub
+
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+        ' Obtener los valores de entrada
+        Dim valorEntrada As Double = Double.Parse(txtValor.Text)
+        Dim tipoUnidad As String = ComboTipounidad.SelectedItem.ToString()
+        Dim unidadEntrada As String = comboUnidadEntrada.SelectedItem.ToString()
+        Dim unidadSalida As String = comboUnidadSalida.SelectedItem.ToString()
+        Dim resultado As Double = 0
+
+        ' Método correspondiente según el tipo de unidad
+        Select Case tipoUnidad
+            Case "Longitud"
+                resultado = ConvertirLongitud(valorEntrada, unidadEntrada, unidadSalida)
+            Case "Masa"
+                resultado = ConvertirMasa(valorEntrada, unidadEntrada, unidadSalida)
+            Case "Tiempo"
+                resultado = ConvertirTiempo(valorEntrada, unidadEntrada, unidadSalida)
+            Case "Almacenamiento"
+                resultado = ConvertirAlmacenamiento(valorEntrada, unidadEntrada, unidadSalida)
+        End Select
+
+        ' Resultado
+        lblResultado.Text = $"Resultado: {resultado}"
+    End Sub
+
+    ' Evento para filtrar unidades según el tipo de unidad seleccionado
+    Private Sub ComboTipounidad_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboTipounidad.SelectedIndexChanged
+        Dim tipoUnidad As String = ComboTipounidad.SelectedItem.ToString()
+
+        ' Limpiar las opciones previas de los ComboBox
+        comboUnidadEntrada.Items.Clear()
+        comboUnidadSalida.Items.Clear()
+
+        ' Activar las listas de unidades al seleccionar un tipo
+        comboUnidadEntrada.Enabled = True
+        comboUnidadSalida.Enabled = True
+
+        ' Agregar las unidades correspondientes al tipo de unidad seleccionado
+        Select Case tipoUnidad
+            Case "Longitud"
+                comboUnidadEntrada.Items.AddRange({"Kilómetros", "Metros", "Centímetros", "Milímetros", "Pulgadas"})
+                comboUnidadSalida.Items.AddRange({"Kilómetros", "Metros", "Centímetros", "Milímetros", "Pulgadas"})
+            Case "Masa"
+                comboUnidadEntrada.Items.AddRange({"Kilogramos", "Gramos", "Miligramos", "Libras"})
+                comboUnidadSalida.Items.AddRange({"Kilogramos", "Gramos", "Miligramos", "Libras"})
+            Case "Tiempo"
+                comboUnidadEntrada.Items.AddRange({"Horas", "Minutos", "Segundos", "Milisegundos"})
+                comboUnidadSalida.Items.AddRange({"Horas", "Minutos", "Segundos", "Milisegundos"})
+            Case "Almacenamiento"
+                comboUnidadEntrada.Items.AddRange({"Kilobytes", "Megabytes", "Gigabytes", "Terabytes"})
+                comboUnidadSalida.Items.AddRange({"Kilobytes", "Megabytes", "Gigabytes", "Terabytes"})
+        End Select
+    End Sub
+
+    ' Métodos de conversión
+
+    Private Function ConvertirLongitud(ByVal valor As Double, ByVal entrada As String, ByVal salida As String) As Double
+        Dim metros As Double = valor
+
+        ' Convertir a metros primero
+        Select Case entrada
+            Case "Kilómetros"
+                metros = valor * 1000
+            Case "Metros"
+                metros = valor
+            Case "Centímetros"
+                metros = valor / 100
+            Case "Milímetros"
+                metros = valor / 1000
+            Case "Pulgadas"
+                metros = valor * 0.0254
+        End Select
+
+        ' Convertir de metros a la unidad de salida
+        Select Case salida
+            Case "Kilómetros"
+                Return metros / 1000
+            Case "Metros"
+                Return metros
+            Case "Centímetros"
+                Return metros * 100
+            Case "Milímetros"
+                Return metros * 1000
+            Case "Pulgadas"
+                Return metros / 0.0254
+            Case Else
+                Return metros
+        End Select
+    End Function
+
+    Private Function ConvertirMasa(ByVal valor As Double, ByVal entrada As String, ByVal salida As String) As Double
+        Dim gramos As Double = valor
+
+        ' Convertir a gramos primero
+        Select Case entrada
+            Case "Kilogramos"
+                gramos = valor * 1000
+            Case "Gramos"
+                gramos = valor
+            Case "Miligramos"
+                gramos = valor / 1000
+            Case "Libras"
+                gramos = valor * 453.592
+        End Select
+
+        ' Convertir de gramos a la unidad de salida
+        Select Case salida
+            Case "Kilogramos"
+                Return gramos / 1000
+            Case "Gramos"
+                Return gramos
+            Case "Miligramos"
+                Return gramos * 1000
+            Case "Libras"
+                Return gramos / 453.592
+            Case Else
+                Return gramos
+        End Select
+    End Function
+
+    Private Function ConvertirTiempo(ByVal valor As Double, ByVal entrada As String, ByVal salida As String) As Double
+        Dim segundos As Double = valor
+
+        ' Convertir a segundos primero
+        Select Case entrada
+            Case "Horas"
+                segundos = valor * 3600
+            Case "Minutos"
+                segundos = valor * 60
+            Case "Segundos"
+                segundos = valor
+            Case "Milisegundos"
+                segundos = valor / 1000
+        End Select
+
+        ' Convertir de segundos a la unidad de salida
+        Select Case salida
+            Case "Horas"
+                Return segundos / 3600
+            Case "Minutos"
+                Return segundos / 60
+            Case "Segundos"
+                Return segundos
+            Case "Milisegundos"
+                Return segundos * 1000
+            Case Else
+                Return segundos
+        End Select
+    End Function
+
+    Private Function ConvertirAlmacenamiento(ByVal valor As Double, ByVal entrada As String, ByVal salida As String) As Double
+        Dim bytes As Double = valor
+
+        ' Convertir a bytes primero
+        Select Case entrada
+            Case "Kilobytes"
+                bytes = valor * 1024
+            Case "Megabytes"
+                bytes = valor * 1024 * 1024
+            Case "Gigabytes"
+                bytes = valor * 1024 * 1024 * 1024
+            Case "Terabytes"
+                bytes = valor * 1024 * 1024 * 1024 * 1024
+        End Select
+
+        ' Convertir de bytes a la unidad de salida
+        Select Case salida
+            Case "Kilobytes"
+                Return bytes / 1024
+            Case "Megabytes"
+                Return bytes / (1024 ^ 2)
+            Case "Gigabytes"
+                Return bytes / (1024 ^ 3)
+            Case "Terabytes"
+                Return bytes / (1024 ^ 4)
+            Case Else
+                Return bytes
+        End Select
+    End Function
+
+End Class
